7d26dd8108f79f5b781cfcf6bafd568c0af8da8c
Jonathan Kwak <jkwaktdchristian@gmail.com>
2025-07-15 19:30:37 -0400

    added icons and some formatting

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sys
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import (
     QApplication,
     QCheckBox,
@@ -22,40 +23,40 @@ from PyQt6.QtWidgets import (
     QTimeEdit,
     QVBoxLayout,
     QWidget,
+    QHBoxLayout
 )
+
+moves = [
+    "rock",
+    "paper",
+    "scissors",
+]
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QComboBox,
-            QDateEdit,
-            QDateTimeEdit,
-            QDial,
-            QDoubleSpinBox,
-            QFontComboBox,
-            QLCDNumber,
-            QLabel,
-            QLineEdit,
-            QProgressBar,
-            QPushButton,
-            QRadioButton,
-            QSlider,
-            QSpinBox,
-            QTimeEdit,
-        ]
 
-        for w in widgets:
-            layout.addWidget(w())
+        central_widget = QWidget() # creating another widget inside a window?
+        layout = QHBoxLayout()
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        widget = QWidget()
-        widget.setLayout(layout)
+        self.setWindowTitle("Rock paper scissors")
+
+        title = QLabel("Choose a move...")
+        layout.addWidget(title)
+
+        for move in moves:
+            widget = QPushButton()
+            widget.setIcon(QIcon(move + ".png"))
+            widget.setIconSize(QSize(80, 80))
+            widget.setFixedSize(100, 100)
+            layout.addWidget(widget)
+
+            
 
         # Set the central widget of the Window.
-        self.setCentralWidget(widget)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
 
 app = QApplication(sys.argv)
